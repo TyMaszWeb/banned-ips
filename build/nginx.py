578499a5b82bcd218ed63e57b73f4d1d1f22cde3
Piotr Kilczuk <piotr@tymaszweb.pl>
2016-05-13 22:53:16 +0100

    Initial commit

---
+++ build/nginx.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+import os.path
+
+
+with open(os.path.join(os.path.dirname(__file__), '..', 'database.txt')) as database:
+    for line in database:
+        print('deny {}'.format(line.strip()))
