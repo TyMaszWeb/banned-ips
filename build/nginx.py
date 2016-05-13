94eb8978e8a8a4a255bffb512d71e1eb6999df00
Piotr Kilczuk <piotr@tymaszweb.pl>
2016-05-13 23:04:19 +0100

    Correct nginx build script

--- build/nginx.py
+++ build/nginx.py
@@ -5,4 +5,4 @@ import os.path
 
 with open(os.path.join(os.path.dirname(__file__), '..', 'database.txt')) as database:
     for line in database:
-        print('deny {}'.format(line.strip()))
+        print('deny {};'.format(line.split('#')[0].strip()))
